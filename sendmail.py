8e62da2bc912f1decea43ed103a66e2b1daa22fb
shivain07 <shivaingusain007@gmail.com>
2025-03-23 01:34:50 +0530

    UPDATED PYTHON CODE

--- sendmail.py
+++ sendmail.py
@@ -1,1 +1,7 @@
-print("hello script print from actions")
+import os
+
+def send_mail(workflow_name,repo_name):
+    print(f"Workflow name {workflow_name} repo name {repo_name}")
+    print(f"DEMO MAIL SENT")
+
+send_mail(os.getenv("WORKFLOW_NAME"),os.getenv("REPO_NAME"))
