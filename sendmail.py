6fbcebeb57bec533a0d1d37555935a1911dad21f
shivain07 <shivaingusain007@gmail.com>
2025-03-23 01:44:08 +0530

    UPDATED emailsender

--- sendmail.py
+++ sendmail.py
@@ -1,7 +1,7 @@
 import os
 
-def send_mail(workflow_name,repo_name):
+def send_mail(workflow_name,repo_name,email,username):
     print(f"Workflow name {workflow_name} repo name {repo_name}")
-    print(f"DEMO MAIL SENT")
+    print(f"DEMO MAIL SENT to {username} via {email}")
 
-send_mail(os.getenv("WORKFLOW_NAME"),os.getenv("REPO_NAME"))
+send_mail(os.getenv("WORKFLOW_NAME"),os.getenv("REPO_NAME"),os.getenv("EMAIL"),os.getenv("USERNAME"))
